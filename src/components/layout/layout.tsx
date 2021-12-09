d66c1f4e14bdb484992458b86a662eb9d4ee6121
cti1650 <cti1650@gmail.com>
2021-12-09 17:17:40 +0000

    背景を透明に

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Layout({ children }) {
   return (
     <>
-      <div className='bg-white w-screen h-screen flex justify-center items-center content-center'>
+      <div className='bg-opacity-0 w-screen h-screen flex justify-center items-center content-center'>
         {children}
       </div>
     </>
