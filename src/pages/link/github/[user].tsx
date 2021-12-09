09bafec42aa1b23dd3e015a38d2a2c17c518b0e6
cti1650 <cti1650@gmail.com>
2021-12-09 16:29:27 +0000

    githubボタン作成

---
+++ src/pages/link/github/[user].tsx
@@ -0,0 +1,33 @@
+
+
+import React, { useState } from 'react';
+import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import { AiFillGithub } from 'react-icons/ai';
+
+const Home = () => {
+  const router = useRouter();
+  const { user } = router.query;
+  const url = 'https://github.com/';
+  return (
+    <div>
+      <Head>
+        <title>Test</title>
+        <meta property='og:title' content='Tailwind CC' />
+      </Head>
+      
+      <span>
+        <a href={url + user}
+        rel="nofollow noreferrer noopener"
+        target="_blank"
+        className="flex flex-row items-center justify-items-center px-8 py-4 bg-gray-300 border border-gray-500 shadow rounded-lg"
+        >
+          <span className="px-0.5"><AiFillGithub /></span><span>{user}</span>
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default Home;
